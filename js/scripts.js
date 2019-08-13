d6e3110b378b1913e35a9e25683e79a0da005dd9
nikhil batta and susan kralian <nikhil@>
2019-08-13 10:41:07 -0700

    add functioning javascript to slide and fade

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $("#fadein1").click(function(){
+    $("#fadein2").fadeIn();
+  });
+  $("#slide1").click(function(){
+    $("#slide2").slideDown();
+  });
+});
+
+// $(document).ready(function(){
+//   $("#slide1").click(function(){
+//     $("#slide2").slideDown();
+//   });
+// });
